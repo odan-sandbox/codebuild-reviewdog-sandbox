2e833dfe6763ea9c827ae248bbed0bc890a1dd15
odan <odan3240@gmail.com>
2020-04-17 01:23:20 +0900

    chore(aws): export variables

--- script/reviewdog.sh
+++ script/reviewdog.sh
@@ -4,15 +4,15 @@
 API_ENDPOINT=https://api.github.com
 AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
 
-CI_REPO_NAME=$(basename $CODEBUILD_SOURCE_REPO_URL)
-CI_REPO_OWNER=$(basename $(dirname $CODEBUILD_SOURCE_REPO_URL))
-CI_COMMIT=$CODEBUILD_RESOLVED_SOURCE_VERSION
+export CI_REPO_NAME=$(basename $CODEBUILD_SOURCE_REPO_URL)
+export CI_REPO_OWNER=$(basename $(dirname $CODEBUILD_SOURCE_REPO_URL))
+export CI_COMMIT=$CODEBUILD_RESOLVED_SOURCE_VERSION
 
-CI_PULL_REQUEST=$(curl -H "$AUTH_HEADER" \
+export CI_PULL_REQUEST=$(curl -H "$AUTH_HEADER" \
   "$API_ENDPOINT/repos/$CI_REPO_OWNER/$CI_REPO_NAME/pulls?head=$CI_REPO_OWNER:$CODEBUILD_WEBHOOK_HEAD_REF" \
   | jq ".[].number")
 
-REVIEWDOG_GITHUB_API_TOKEN=$GITHUB_TOKEN
+export REVIEWDOG_GITHUB_API_TOKEN=$GITHUB_TOKEN
 
 # Download
 curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s
